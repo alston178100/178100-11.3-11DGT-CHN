efa1f219194554696b0b6d453bf72ebd28fb1a2d
Alston <alstonhuang598@gmail.com>
2025-09-02 13:32:32 +1200

    committed 2/9

--- Python/11.3-Assessment/game1_wordle.py
+++ Python/11.3-Assessment/game1_wordle.py
@@ -7,6 +7,52 @@ word = f.readlines()
 for i in range(len(word)):
     word[i] = word[i].strip()
 
-print(word[random.randint(0, len(word) - 1)])
+target_word = word[random.randint(0, len(word) - 1)]
+print(target_word)
 
+user_word = ""
+attempts = 0
+guessed = False
 
+while attempts < 6:
+    letter_inp = letter_inp = input("Enter letter: ")
+    while letter_inp != "]":
+        if letter_inp == "[":
+            if len(user_word) >= 1:
+                user_word = user_word[:-1]
+            else:
+                print("No letters")
+        else:
+            if len(user_word) < 5:
+                user_word += letter_inp
+            else:
+                print("Too many letters")
+        print(user_word)
+        letter_inp = input("Enter letter: ")
+    
+    if len(user_word) != 5:
+        print("Entered but not enough letters")
+    elif user_word not in word:
+        print("Invalid word")
+    else:
+        attempts += 1
+    
+        if user_word == target_word:
+            print(target_word)
+            print("Correct word")
+            break
+        else:
+            cond = ""
+            for i in range(5):
+                if user_word[i] == target_word[i]:
+                    cond += "2"
+                elif user_word[i] in target_word:
+                    cond += "1"
+                else:
+                    cond += "0"
+            print(user_word)
+            print(cond)
+            print("Not the word")
+        user_word = ""
+
+print(attempts)
--- Python/11.3-Testing/hello.py
+++ Python/11.3-Testing/hello.py
@@ -3,3 +3,26 @@ print("Hi")
 
 for i in range(1000):
     print(str(i))
+
+li = []
+for i in range(10):
+    li.append([0] * 10)
+
+li[0][1] = -1
+li[3][8] = -1
+x = 1
+y = 6
+
+li[x][y] = -1
+
+li[x-1][y-1] += 1
+li[x][y-1] += 1
+li[x+1][y-1] += 1
+li[x-1][y] += 1
+li[x+1][y] += 1
+li[x-1][y+1] += 1
+li[x][y+1] += 1
+li[x+1][y+1] += 1
+
+for i in li:
+    print(i)
