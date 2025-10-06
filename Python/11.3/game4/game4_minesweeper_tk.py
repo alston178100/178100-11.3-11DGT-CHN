b668e6531f361e4951e4a5c66e04e3c0f111b338
Alston <alstonhuang598@gmail.com>
2025-10-07 09:41:08 +1300

    7/10 game 3 flags left display version

--- Python/11.3/game4/game4_minesweeper_tk.py
+++ Python/11.3/game4/game4_minesweeper_tk.py
@@ -70,6 +70,7 @@ def RevealLost(r, c):
                                 background="white")
 
 def RevealButton(r, c):
+    global safe_squares_revealed
     globals()["button_" + str(temp_button_coords[0]) + 
               "_" + str(temp_button_coords[1])].config(
                   background="SystemButtonFace")
@@ -107,10 +108,12 @@ def RevealButton(r, c):
             if c != 15:
                 if not revealed[r][c+1]:
                     RevealButton(r, c+1)
+            safe_squares_revealed += 1
         elif sweeper_grid[r][c] != 9:
             Label(mines_frame, text=sweeper_grid[r][c], 
                   background="white", fg=colours[sweeper_grid[r][c]-1]
                   ).grid(row=r, column=c)
+            safe_squares_revealed += 1
         else:
             bg_img = Label(mines_frame, image=PhotoImage(width=19, height=19), 
                         background="red")
@@ -118,16 +121,23 @@ def RevealButton(r, c):
             Label(mines_frame, text="💣", background="red"
                   ).grid(row=r, column=c)
             g_root.after(10, lambda: RevealLost(r, c))
+        if safe_squares_revealed == 216:
+            print("WIN")
 
 def ChangeFlag(r, c):
+    global flags_left
     if flag_placed[r][c]:
-        globals()["button_" + str(r) + "_" + str(c)].config(text="", padx=0, pady=0)
+        globals()["button_" + str(r) + "_" + str(c)].config(text="", padx=0, 
+                                                            pady=0)
         flag_placed[r][c] = 0
+        flags_left += 1
     else:
         globals()["button_" + str(r) + "_" + str(c)
                   ].config(text="🚩", compound=CENTER, padx=0, pady=0,
                            font=("Arial", 9))
         flag_placed[r][c] = 1
+        flags_left -= 1
+    flags_left_text.config(text=flags_left)
 
 # This function is necessary since i and j are dynamic variables
 def EditButtonCommands(r, c):
@@ -145,6 +155,7 @@ flag_placed = []
 for i in range(16):
     revealed.append([0] * 16)
     flag_placed.append([0] * 16)
+safe_squares_revealed = 0
 
 sweeper_grid = GetGrid()
 
@@ -152,6 +163,14 @@ sweeper_grid = GetGrid()
 
 Label(g_root, text="MINESWEEPER", font=("Times New Roman", 36)).pack(
     pady=(20, 10))
+
+flags_left = 40
+upper_frame = Frame(g_root)
+upper_frame.pack(pady=10)
+
+Label(upper_frame, text="Flags Left").grid(row=0, column=0)
+flags_left_text = Label(upper_frame, text=flags_left)
+flags_left_text.grid(row=1, column=0)
 
 mines_frame = Frame(g_root, width=150, height=150)
 mines_frame.pack()
@@ -170,16 +189,16 @@ for i in range(16):
 break_i_loop = False
 pixel_img_2 = PhotoImage(width=20, height=20)
 for i in range(16):
-    for j in range(4):
-        if sweeper_grid[8-i][9-j] == 0:
+    for j in range(6):
+        if sweeper_grid[8-i][10-j] == 0:
             if i > 8:
-                globals()["button_" + str(16-abs(8-i)) + "_" + str(9-j)
+                globals()["button_" + str(16-abs(8-i)) + "_" + str(10-j)
                           ].config(image=pixel_img_2)
-                temp_button_coords = (16-abs(8-i), 9-j)
+                temp_button_coords = (16-abs(8-i), 10-j)
             else:
-                globals()["button_" + str(8-i) + "_" + str(9-j)
+                globals()["button_" + str(8-i) + "_" + str(10-j)
                           ].config(background="yellow")
-                temp_button_coords = (8-i, 9-j)
+                temp_button_coords = (8-i, 10-j)
             break_i_loop = True
             break
     if break_i_loop:
